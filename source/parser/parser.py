db5988e4b1a54778ee2f1fca26483feb1b060115
Jan Pajdak <pajdakjan@gmail.com>
2019-06-01 22:45:30 +0200

    Change parser to look for comprehensive/regular 16s

--- source/parser/parser.py
+++ source/parser/parser.py
@@ -10,12 +10,15 @@ from os.path import isfile, join
 #       some specials
 #       ASCII codes allowed: <33,46> + <48,57> + <63,90> + <94, 122>
 input_path = 'G:/BreachCompilation/data'
-output_path = '../../resources/passwords/real'
+output_path = 'G:/BreachCompilation/real'
+#output_path = '../../resources/passwords/real'
 raw_passwords = [[], 0]
 regular8_passwords = [[], 0]
+regular16_passwords = [[], 0]
 #nospecial8_passwords = [[], 0]
 comprehensive8_passwords = [[], 0]
-passwords_per_file = 100000
+comprehensive16_passwords = [[], 0]
+passwords_per_file = 500000
 
 
 def parse_file(input_path):
@@ -38,14 +41,31 @@ def parse_line(line):
         return None
 
 
+# old method
+#def filter_password(password):
+#    global raw_passwords
+#    global regular8_passwords
+#    #global nospecial8_passwords
+#    global comprehensive8_passwords
+#    raw_passwords[0].append(password)
+#    if (len(password.strip()) >= 8):
+#        regular8_passwords[0].append(password)
+#        # !@#$%^&*()-_=+[]{};’:”,./<>?
+#        if re.search('[0-9]', password) is None:
+#            return
+#        if re.search('[A-Z]', password) is None:
+#            return
+#        if re.search('[!@#$%^&*\-_=+’”,\/.?]', password) is None:
+#            return
+#        comprehensive8_passwords[0].append(password)
+
+
+# quick shitpatch to get 16s
 def filter_password(password):
-    global raw_passwords
-    global regular8_passwords
-    #global nospecial8_passwords
-    global comprehensive8_passwords
-    raw_passwords[0].append(password)
-    if (len(password.strip()) >= 8):
-        regular8_passwords[0].append(password)
+    global regular16_passwords
+    global comprehensive16_passwords
+    if (len(password.strip()) >= 16):
+        regular16_passwords[0].append(password)
         # !@#$%^&*()-_=+[]{};’:”,./<>?
         if re.search('[0-9]', password) is None:
             return
@@ -53,7 +73,7 @@ def filter_password(password):
             return
         if re.search('[!@#$%^&*\-_=+’”,\/.?]', password) is None:
             return
-        comprehensive8_passwords[0].append(password)
+        comprehensive16_passwords[0].append(password)
 
 
 def count_lines_in_file(file):
@@ -101,9 +121,9 @@ def parse_directory(dir_path):
         else:
             print(f'nested folder {content_path}')
             parse_directory(content_path)
-        write_passwords('raw', raw_passwords)
-        write_passwords('regular8', regular8_passwords)
-        write_passwords('comprehensive8', comprehensive8_passwords)
+        #write_passwords('raw', raw_passwords)
+        write_passwords('regular16', regular16_passwords)
+        write_passwords('comprehensive16', comprehensive16_passwords)
 
 
 def main():
